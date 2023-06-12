ce84af38decaf18297e61b190bdf7b60029ce4c5
Kris Quigley <kris@krisquigley.co.uk>
2023-06-12 12:57:24 +0100

    feat: uses docker for postgres only

---
+++ dist/config/env/development/database.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = ({ env }) => ({
+    connection: {
+        client: 'postgres',
+        connection: {
+            host: env('DATABASE_HOST', 'localhost'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME', 'community-strapi'),
+            user: env('DATABASE_USERNAME', 'strapi'),
+            password: env('DATABASE_PASSWORD', 'z99qN4zZVxt9O14P12813124Ad6'),
+            ssl: env.bool('DATABASE_SSL', false)
+        }
+    }
+});
