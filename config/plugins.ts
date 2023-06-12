e09e97acb6598186dbb5beced3136963dffa4c84
Kris Quigley <kris@krisquigley.co.uk>
2023-06-12 21:26:35 +0100

    feat: adds postmark email provider

---
+++ config/plugins.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: "strapi-provider-email-postmark",
+      providerOptions: {
+        apiKey: env("POSTMARK_API_KEY", 'random-key'),
+      }, 
+      settings: {
+        defaultFrom: env("POSTMARK_REPLY_FROM_ADDRESS", 'random-key'),
+      },
+    }
+  },
+});
